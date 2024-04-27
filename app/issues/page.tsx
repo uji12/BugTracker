6d0aedeee68251a26e4ff4e2e0c0389b1830b5c1
maababa <ujjwal1899@gmail.com>
2024-04-28 01:33:32 +0530

    set up radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@radix-ui/themes'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <Button>
+      Click here ➡️
+    </Button>
   )
 }
 
